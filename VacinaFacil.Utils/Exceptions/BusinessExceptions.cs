c409522c244d1f59b8f8c06296d8d6a691ea3fd6
PedroCalabria <pedsancal@gmail.com>
2024-07-09 11:23:46 -0300

    #9 Implemented exceptions and log
    - Implemented exception's class
    - Created error messages' resources

---
+++ VacinaFacil.Utils/Exceptions/BusinessExceptions.cs
@@ -0,0 +1,11 @@
+﻿namespace VacinaFacil.Utils.Exceptions
+{
+    public class BusinessException : Exception
+    {
+        public BusinessException() { }
+
+        public BusinessException(string message) : base(message) { }
+
+        public BusinessException(string message, Exception exception) : base(message, exception) { }
+    }
+}
