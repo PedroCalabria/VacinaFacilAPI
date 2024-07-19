871e340302b24e4f1afe86bf62073404f069a91d
PedroCalabria <pedsancal@gmail.com>
2024-07-19 17:31:42 -0300

    #29 Implemented AppointmentPatientController

---
+++ VacinaFacil.Api/Controllers/AppointmentPatientController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using VacinaFacil.Business.Interface.IBusinesses;
+using VacinaFacil.Entity.DTO;
+
+namespace VacinaFacil.Api.Controllers
+{
+    [ApiController]
+    [Authorize]
+    [Route("api/[controller]")]
+    public class AppointmentPatientController : ControllerBase
+    {
+        private readonly IAppointmentPatientBusiness _appointmentpatientBusiness;
+
+        public AppointmentPatientController(IAppointmentPatientBusiness appointmentpatientBusiness)
+        {
+            _appointmentpatientBusiness = appointmentpatientBusiness;
+        }
+
+        [HttpGet("GetListAppointmentsPatients")]
+        public async Task<List<GroupedAppointmentPatientDTO>> GetListAppointmentsPatients()
+        {
+            return await _appointmentpatientBusiness.ListAppointments();
+        }
+
+        [HttpGet("GetListAppointmentsPatientsByDate")]
+        public async Task<List<GroupedAppointmentPatientDTO>> GetListAppointmentsPatientsByDate(DateTime date)
+        {
+            return await _appointmentpatientBusiness.ListAppointmentsByDate(date);
+        }
+    }
+}
